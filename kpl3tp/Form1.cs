c6eda85a5a2dfd7ad2399afefe6f5b56642da260
Marjauza Naswansyah <marjauzann@gmail.com>
2024-03-03 20:47:41 +0700

    Add project files.

---
+++ kpl3tp/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace kpl3tp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSubmit_Click(object sender, EventArgs e)
+        {
+            string namaPraktikan = textBoxInput.Text;
+
+            if (!string.IsNullOrWhiteSpace(namaPraktikan))
+            {
+                labelOutput.Text = "Halo " + namaPraktikan + " ";
+            }
+            else
+            {
+                MessageBox.Show("Mohon isi nama praktikan terlebih dahulu!", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
